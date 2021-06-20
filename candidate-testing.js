f42d8b0e08bd601cc6f4daa6ed5bbf0bd0f5f75e
edlopez000 <85708427+edlopez000@users.noreply.github.com>
2021-06-20 06:36:35 +0000

    Program now asks multiple questions and stores answers. Implemented loops and arrays, and a grading system.

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,10 +8,12 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer= "";
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space?", "True or false: 5 kilometer == 5000 meters?", "(5 + 3)/2 * 10 = ?", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
+let correctAnswers = ["Sally Ride", "true", 40, "Trajectory", 3];
+let candidateAnswers = [];
 
+let numberOfQuestions = questions.length;
+let numberOfCorrect = 0;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -20,22 +22,29 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question);
+  // candidateAnswer = input.question(question);
 
+  for (let i = 0; i < questions.length; i++) {
+    candidateAnswers[i] = String(input.question(questions[i])).toLowerCase();
+  }
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
-  if (candidateAnswer == correctAnswer) {
-    console.log("You have answered correctly!");
-  } else {
-    console.log("You have answered incorrectly!");
+  for (let i = 0; i < candidateAnswers.length; i++) {
+    console.log(`Your answer: ${candidateAnswers[i]}\nThe correct answer: ${correctAnswers[i]}`);
+  }
+  
+  for (let i = 0; i < questions.length; i++) {
+    if (candidateAnswers[i] == String(correctAnswers[i]).toLowerCase()) {
+      numberOfCorrect += 1;
+    }
   }
 
-  let grade;
-  
+  let grade = (numberOfCorrect / numberOfQuestions) * 100;
+  console.log(`>>> Overall Grade: ${grade}% (${numberOfCorrect} of ${numberOfQuestions} responses correct) <<<`)
 
   return grade;
 }
